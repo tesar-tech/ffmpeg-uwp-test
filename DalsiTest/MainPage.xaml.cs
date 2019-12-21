66dc2482442cb7a03ee1e8fafd6e1ff315cf5bd5
tesar-tech <jan@tesar.tech>
2019-12-21 16:41:07 +0100

    clean up

--- DalsiTest/MainPage.xaml.cs
+++ DalsiTest/MainPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
+using Windows.Graphics.Imaging;
 using Windows.Media.Playback;
 using Windows.Storage;
 using Windows.Storage.Streams;
@@ -25,55 +26,43 @@ namespace DalsiTest
         //base project https://github.com/ffmpeginteropx/FFmpegInteropX
         //builded nuget https://www.nuget.org/packages/Hyvart.FFmpegInteropX/
         //ffmpeg framegrabber ffmpeg -i beru.wmv -r 25 $filename%03d.bmp
+
+        //frame grabber sample: https://github.com/ffmpeginteropx/FFmpegInteropX/blob/3d0e2cfd6c64f67b339637e27b543bb5d95398fc/Tests/TestExtractThumbnail.cs
+
+        private StorageFile currentFile;
+        private IRandomAccessStream stream;
+        FrameGrabber frameGrabber;
+        Stopwatch sw;
+
+
         public MainPage()
         {
             this.InitializeComponent();
             sw = new Stopwatch();
         }
-        private void Page_Loaded(object sender, RoutedEventArgs e)
+        private async void Page_Loaded(object sender, RoutedEventArgs e)
         {
-            GetSF();//initialize file, stream and frame grabber
-        }
-
-        private StorageFile currentFile;
-        private IRandomAccessStream stream;
-        FrameGrabber frameGrabber;
-
-        private async void GetSF()
-        {
+            //initialize file, stream and frame grabber
             currentFile = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///Assets/beru.wmv"));
             stream = await currentFile.OpenAsync(FileAccessMode.Read);
             frameGrabber = await FrameGrabber.CreateFromStreamAsync(stream);
         }
 
-        private async Task ExtractFrame(TimeSpan position)
-        {
-            //await Task.Delay(1000);
-                //var sws = new Stopwatch();
-                //sws.Start();
-                var frame = await frameGrabber.ExtractVideoFrameAsync(position, true);
-                //Debug.WriteLine(sws.ElapsedMilliseconds);
-            //read data:
-            //var pixData = frame.PixelData.ToArray();
-            //Debug.WriteLine(pixData[10500]);
-        }
-
-        Stopwatch sw;
         private async void Button_Click(object sender, RoutedEventArgs e)
         {
-            sw.Reset();
-            sw.Start();
+            sw.Restart();
             int numOfFrames = (int)Math.Floor(mpe.MediaPlayer.PlaybackSession.NaturalDuration.TotalSeconds * 25);
             TimeSpan currentPosition = TimeSpan.Zero;
 
             for (int i = 0; i < numOfFrames; i++)
             {
-                currentPosition += TimeSpan.FromMilliseconds(1000d / 25d);//every frame
-                await ExtractFrame(currentPosition);
+                currentPosition += TimeSpan.FromMilliseconds(1000d / 25d);//every frame (fps is 25 for test video)
+                var frame = await frameGrabber.ExtractVideoFrameAsync(currentPosition, true);
+                Debug.WriteLine($"Frame from {currentPosition.TotalSeconds} sec. Elapsed:{sw.Elapsed.TotalSeconds} seconds");
             }
             Debug.WriteLine($"All ({numOfFrames}) in {sw.Elapsed.TotalSeconds} seconds");
         }
     }
 
-   
+
 }
